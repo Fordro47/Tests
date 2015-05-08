dd915668d03c44c3a514c8141ef0a1a887a26db8
Fordro47 <Fordro47@gmail.com>
2015-05-08 14:08:47 -0400

    added another class

--- FirstTest.java
+++ FirstTest.java
@@ -2,5 +2,8 @@
 public class FirstTest{
 	public static void main (String[] args){
 		System.out.println("This is my first test");
+		SecondTest sT = new SecondTest();
+		int answer = sT.add(1, 2);
+		System.out.print(answer);
 	}
 }
---
+++ SecondTest.java
@@ -0,0 +1,9 @@
+public class SecondTest{
+	public SecondTest(){
+
+	}
+	public int add(int a, int b){
+		int result = a + b;
+		return result;
+	}
+}
